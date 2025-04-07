3753d3bcd61bd4d48ad59132811f8a50fba5d2e9
v3ronez <v3ronez.dev@gmail.com>
2025-04-07 13:35:41 -0300

    sketchybar

---
+++ sketchybar/items/calendar.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+calendar=(
+  icon=cal
+  icon.font="$FONT:Black:12.0"
+  icon.padding_right=0
+  label.width=65
+  label.align=right
+  padding_left=15
+  update_freq=30
+  script="$PLUGIN_DIR/calendar.sh")
+
+  sketchybar --add item calendar right       \
+           --set calendar "${calendar[@]}"   \
+           --subscribe calendar system_woke
